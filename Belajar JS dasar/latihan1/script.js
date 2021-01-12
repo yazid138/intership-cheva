82b4a4fcba635a09ded0cbb5d0ebb7e45ce6f6ef
Ma'isy Yazid I <ma39isy@gmail.com>
2021-01-12 14:58:15 +0700

    belajar js1

---
+++ Belajar JS dasar/latihan1/script.js
@@ -0,0 +1,15 @@
+let a = 5
+let b = 10
+let c = a + b
+
+console.log(c)
+
+if (a === b) {
+    console.log(a)
+} else {
+    console.log(b)
+}
+
+for (let i = 0; i < a; i++) {
+    console.log(i)
+}
